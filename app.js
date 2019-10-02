cefc157501a9bfd9ad4545756e892b4824750c60
zcpefaisal <inboxlions@gmail.com>
2019-10-02 19:35:01 +0600

    data binding, event, modifier practis

--- app.js
+++ app.js
@@ -2,13 +2,39 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Faisal',
-        job: 'ninja'
+        age: 28,
+        job: 'ninja',
+        website: 'http://faisalalam.me',
+        websiteTag: '<a href="http://faisalalam.me">Web</a>',
+        x: 0,
+        y: 0,
+        salary: ''
     },
     methods: {
         greet: function(param) {
             // return time;
             // return this.name;
             return 'Good ' + param + ' ! ' + this.name;
+        },
+        add: function(inc) {
+            this.age += inc;
+        },
+        substruct: function(des) {
+            this.age -= des;
+        },
+        updateXY: function(event) {
+            // console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        click: function() {
+            alert('clicked this link');
+        },
+        logName: function() {
+            console.log('you entered your name');
+        },
+        logAge: function() {
+            console.log('you entered your age');
         }
     }
 })
